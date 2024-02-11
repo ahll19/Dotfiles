39a54f70ba3f90e26ad8830e2ea966d6c5714317
Anders Lauridsen <ahll19@student.aau.dk>
2024-02-11 17:03:47 +0100

    Added missing pyenv to dependency

--- Scripts/zsh/zshrc_dependency.sh
+++ Scripts/zsh/zshrc_dependency.sh
@@ -11,6 +11,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo "Installing oh-my-zsh dependencies"
 curl -#fLo- 'https://raw.githubusercontent.com/hyperupcall/autoenv/master/scripts/install.sh' | sh
 sudo pacman -S xclip github-cli thefuck tmux acpi --noconfirm
+sudo pacman -S pyenv
+yay -S pyenv-virtualenv
 
 echo "Installing and running stow"
 sudo pacman -S stow
